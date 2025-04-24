a753092043e876d14bedb7946b9b85a4129a951d
Nick Cotton <nick.c.cotton@gmail.com>
2025-04-24 10:16:00 +1000

    Format size and performance for readability

--- src/App.vue
+++ src/App.vue
@@ -16,23 +16,27 @@
     <div v-else-if="results.length">
       {{ total }} results found
       <ul class="grid gap-3">
-        <li v-for="result in results" :key="result.symbol" class="bg-white p-4 border rounded-lg">
-          <div class="flex gap-3">
+        <li
+          class="bg-card text-card-foreground p-4 gap-6 rounded-xl border shadow-sm"
+          v-for="result in results"
+          :key="result.symbol"
+        >
+          <div class="flex gap-4">
             <div class="flex-1">
               <p>{{ result.symbol }}</p>
               <h2>{{ result.display_name }}</h2>
             </div>
             <div class="">
               <p>Size</p>
-              <p>{{ result.fund_size }}</p>
+              <p>{{ formatSize(result.fund_size) }}</p>
             </div>
             <div class="">
               <p>1y</p>
-              <p>{{ result.one_year_return }}</p>
+              <p>{{ formatPerformance(result.one_year_return) }}</p>
             </div>
             <div class="">
               <p>5y</p>
-              <p>{{ result.five_year_return }}</p>
+              <p>{{ formatPerformance(result.five_year_return) }}</p>
             </div>
           </div>
         </li>
@@ -64,5 +68,25 @@ const handleSearchInput = (event: Event) => {
 
 const submitSearch = () => {
   search({ search_text: query.value })
+}
+
+const formatSize = (value: string | null | undefined): string => {
+  if (value == null) return 'N/A'
+
+  const num = parseFloat(value)
+  if (isNaN(num)) return 'N/A'
+
+  const size = num * 1_000_000
+
+  if (size >= 1e9) return (size / 1e9).toFixed(2) + 'B'
+  if (size >= 1e6) return (size / 1e6).toFixed(2) + 'M'
+  if (size >= 1e3) return (size / 1e3).toFixed(2) + 'K'
+
+  return size.toFixed(2)
+}
+
+const formatPerformance = (value: string) => {
+  if (value === null || value === undefined) return 'N/A'
+  return parseFloat(value).toFixed(2) + '%'
 }
 </script>
